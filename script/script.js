295894e180c37f06ee8acee9c3afdb3dd2a3346b
dibyo007dev <dibyomails007@gmail.com>
2016-09-15 19:23:58 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,26 @@
+    var content = document.getElementById("content");
+    var button = document.getElementById("show-more");
+
+    button.onclick = function(){
+
+       if(content.className == "open") {
+         // shrink the box
+         content.className="";
+         button.innerHTML="Show More";
+       }
+       else {
+         // open the box
+         content.className= "open";
+         button.innerHTML="Show Less";
+
+       }
+    }
+
+  $(document).ready(function(){
+console.log("start");
+
+$('.image').slideIn(500).css({
+  'border' : '2px red solid'
+});
+
+ });
